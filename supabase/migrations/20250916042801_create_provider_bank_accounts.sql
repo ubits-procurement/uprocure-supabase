a7631fca69f275f8db8283afe994562079a8ee1e
Procurement <procurement@ubits.co>
2025-09-15 23:34:23 -0500

    fix: add public prefix on migration

--- supabase/migrations/20250916042801_create_provider_bank_accounts.sql
+++ supabase/migrations/20250916042801_create_provider_bank_accounts.sql
@@ -1,4 +1,4 @@
-CREATE TABLE provider_bank_accounts (
+CREATE TABLE public.provider_bank_accounts (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     provider_id STRING NOT NULL REFERENCES providers(nit) ON DELETE CASCADE,
     data JSONB NOT NULL,
@@ -6,9 +6,9 @@ CREATE TABLE provider_bank_accounts (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 );
 
-COMMENT ON TABLE provider_bank_accounts IS 'Tabla que contiene las cuentas bancarias de los proveedores';
-COMMENT ON COLUMN provider_bank_accounts.id IS 'Identificador único de la cuenta bancaria';
-COMMENT ON COLUMN provider_bank_accounts.provider_id IS 'NIT del proveedor asociado a la cuenta bancaria';
-COMMENT ON COLUMN provider_bank_accounts.data IS 'Datos de la cuenta bancaria en formato JSONB';
-COMMENT ON COLUMN provider_bank_accounts.created_at IS 'Fecha de creación del registro';
-COMMENT ON COLUMN provider_bank_accounts.updated_at IS 'Fecha de la última actualización del registro';
+COMMENT ON TABLE public.provider_bank_accounts IS 'Tabla que contiene las cuentas bancarias de los proveedores';
+COMMENT ON COLUMN public.provider_bank_accounts.id IS 'Identificador único de la cuenta bancaria';
+COMMENT ON COLUMN public.provider_bank_accounts.provider_id IS 'NIT del proveedor asociado a la cuenta bancaria';
+COMMENT ON COLUMN public.provider_bank_accounts.data IS 'Datos de la cuenta bancaria en formato JSONB';
+COMMENT ON COLUMN public.provider_bank_accounts.created_at IS 'Fecha de creación del registro';
+COMMENT ON COLUMN public.provider_bank_accounts.updated_at IS 'Fecha de la última actualización del registro';
